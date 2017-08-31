b4676042bf5b452d0a8556c055169e0365d92cf2
Anderson Muniz <muniz710@hotmail.com>
2017-08-31 16:25:36 -0300

    Graduacao, Calibres e Armas funcionando!

--- bin/import.sql
+++ bin/import.sql
@@ -1,2 +1,6 @@
 insert into Graduacoes values(1,'Recruta', 'RE');
 insert into Graduacoes values(2,'Soldado', 'SD');
+insert into Graduacoes values(3,'Cabo', 'CB');
+insert into Graduacoes values(4,'Sargento', 'SGT');
+
+insert into Calibres values(1,'762');
