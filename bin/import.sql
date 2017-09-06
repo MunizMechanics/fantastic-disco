381bfd16f4b8b0de9466ef8b5f60a7d8240ea2c2
Anderson Muniz <muniz710@hotmail.com>
2017-09-06 15:10:18 -0300

    Menu em todas as paginas

--- bin/import.sql
+++ bin/import.sql
@@ -1,17 +1,16 @@
 insert into Graduacoes values(1,'Recruta', 'RE');
 insert into Graduacoes values(2,'Soldado', 'SD');
 insert into Graduacoes values(3,'Cabo', 'CB');
-insert into Graduacoes values(4,'Sargento', 'SGT');
-insert into Graduacoes values(5,'Capitão', 'CAP');
-insert into Graduacoes values(6,'Tenete', 'TEN');
 
 insert into Calibres values(1,'762');
-insert into Calibres values(2,'380');
-insert into Calibres values(3,'12');
 
-insert into Armas(idarma,calibre,nomearma) values(1, 1, 'Fuzil213');
-insert into Armas(idarma,calibre,nomearma) values(2, 1, 'Fuzil214');
-insert into Armas(idarma,calibre,nomearma) values(3, 3, 'Carabina');
+insert into tipomovimentacoes values(1, 'Empréstimo');
+insert into tipomovimentacoes values(2, 'Devolução');
 
-insert into Militares(idmilitar, graduacao, nomemilitar) values(1, 5, 'Leivas');
-insert into Militares(idmilitar, graduacao, nomemilitar) values(2, 6, 'Muniz');
+insert into Armas(idarma,calibre,nomearma) values(1, 1, 'Fuzil1');
+insert into Armas(idarma,calibre,nomearma) values(2, 1, 'Fuzil2');
+insert into Armas(idarma,calibre,nomearma) values(3, 1, 'Fuzil3');
+
+insert into Militares(idmilitar, graduacao, nomemilitar) values(1, 2, 'Leivas');
+insert into Militares(idmilitar, graduacao, nomemilitar) values(2, 3, 'Muniz');
+
